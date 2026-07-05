f209c9dceb63b1590e340cc51aad4bdac6664ed7
jmarino13 <Jude@lmgmedicine.com>
2026-07-05 18:55:00 -0500

    Add providers nav link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,6 +11,7 @@ export default function Navbar() {
         <Link href="/">Home</Link>
         <Link href="/technology">Technology</Link>
         <Link href="/products">Products</Link>
+        <Link href="/providers">Providers</Link>
         <Link href="/cart">Cart</Link>
       </nav>
 
